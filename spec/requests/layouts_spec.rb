b8579de5278f8a612b559c2d61d68359ea7a64da
Johnny Hom <johnnyhom@gmail.com>
2011-11-26 12:13:05 -0800

    Added a Users controller and layout links tests

--- spec/requests/layouts_spec.rb
+++ spec/requests/layouts_spec.rb
@@ -21,4 +21,13 @@ describe "LayoutLinks" do
     get '/help'
     response.should have_selector('title', :content => "Help")
   end
+  
+  require 'spec_helper'
+
+  it "should have a signup page at '/signup'" do
+    get '/signup'
+    response.should have_selector('title', :content => "Sign up")
+  end
+end
+
 end
