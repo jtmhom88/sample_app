e22caf9939e54dda3f6645602a087d1fa3d4ff9d
Johnny Hom <johnnyhom@gmail.com>
2011-12-24 08:27:37 -0800

    Microposts interface

---
+++ app/controllers/microposts_controller.rb
@@ -0,0 +1,17 @@
+class MicropostsController < ApplicationController
+  before_filter :authenticate, :only => [:create, :destroy]
+  before_filter :authorized_user, :only => :destroy
+  
+  def create
+    @micropost  = current_user.microposts.build(params[:micropost])
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+      redirect_to root_path
+    else
+      render 'pages/home'
+    end
+  end
+
+  def destroy
+  end
+end
